ada0c632f806ca9bc2b99bfe71b2a1f9cde6945e
Hentsuna <damtrongj095@gmail.com>
2023-03-28 10:04:56 +0700

    changes

--- DSPhanSo/Program.cs
+++ DSPhanSo/Program.cs
@@ -8,26 +8,20 @@ namespace Bai2_2
         private int tuSo;
         public int TuSo
         {
-            get{ return tuSo;}
-            set{ tuSo = value;} 
+            get { return tuSo; }
+            set { tuSo = value; }
         }
-         private int mauSo;
-         public int MauSo
-         {
-            get{ return mauSo;}
-            set{ mauSo= value;}
-         }
-        
+        private int mauSo;
+        public int MauSo
+        {
+            get { return mauSo; }
+            set { mauSo = value; }
+        }
+
         public PhanSo(int ts, int ms)
         {
             tuSo = ts;
             mauSo = ms;
-        }
-        
-        PhanSo(PhanSo p)
-        {
-            tuSo = p.tuSo;
-            mauSo = p.mauSo;
         }
 
         public void Nhap()
@@ -36,76 +30,99 @@ namespace Bai2_2
             TuSo = Convert.ToInt32(Console.ReadLine());
             Console.Write("Nhap mau so: ");
             do
-            MauSo = Convert.ToInt32(Console.ReadLine());
-            while(MauSo==0); 
+                MauSo = Convert.ToInt32(Console.ReadLine());
+            while (MauSo == 0);
         }
 
         public void Xuat()
         {
-            Console.WriteLine("Phan so da nhap la: {0}/{1}",TuSo,MauSo);
+            Console.WriteLine("{0}/{1}", TuSo, MauSo);
         }
-
-        public void ToiGian()
+        public double DbPS()
         {
-            int a = TuSo;
-            int b = MauSo;
-            while(a!=b)
-            {
-                if(a>b) a-=b;
-                else b-=a;
-            }
-            TuSo/=a;
-            MauSo/=b;
-            Console.WriteLine("Phan so sau khi toi gian: {0}/{1}",TuSo,MauSo);
+            return (double)tuSo / mauSo;
         }
     }
 
+    //Khai bao lop danh sach phan so
     public class DSPhanSo
     {
         private int _size;
         public int Size
-        { 
-            get {return _size;} 
-            set {_size = value;} 
-        } // So luong phan so
-
-        private PhanSo[] _dsPs;
-        public int DSPS
-        { 
-            get;
-            set; 
+        {
+            get{return _size;}
+            set{_size = value;}
         }
 
-        public void NhapDS()
+        PhanSo[] DSPS;
+        
+        public void Nhap()
         {
-            Console.WriteLine("Nhap so luong phan tu phan so: ");
-            Size=int.Parse(Console.ReadLine());
-            DSPS = new PhanSo[Size];
-            for(int i=0; i<Size; i++)
+            Console.Write("So luong phan so: ");
+            _size = Convert.ToInt32(Console.ReadLine());
+            //Nhap mang phan so
+            DSPS = new PhanSo[_size];
+            for(int i=0; i<_size; i++)
             {
-                Console.WriteLine("Phan so thu {0} ",i);
-                DSPS[i] = new PhanSo();
+                Console.WriteLine("Phan so thu {0} ", i+1);
+                DSPS[i] = new PhanSo(0,0);
                 DSPS[i].Nhap();
             }
         }
 
-        public void XuatDS()
+        public void Xuat()
         {
-            Console.WriteLine("Danh sach phan so: \t");
-            for(int i=0; i<Size; i++) DSPS[i].Xuat();
+            Console.WriteLine("Danh sach phan so: ");
+            for(int i=0; i<_size; i++) DSPS![i].Xuat();
         }
 
+        //Tim phan so lon nhat
+        public void Max()
+        {   
+
+            double max = DSPS[0].DbPS();
+            int count = 0;
+            for(int i=1; i<_size; i++)
+            {
+                if(DSPS[i].DbPS() > max)
+                { 
+                    max = DSPS[i].DbPS();
+                    count = i;
+                }
+            }
+            Console.Write("Phan so lon nhat la: ");
+            DSPS[count].Xuat();
+        }
+
+        public void Adv()
+        {
+            for(int i=0; i<_size-1; i++)
+            {
+                for(int j=1; j<_size; j++)
+                {
+                    if(DSPS[i].DbPS() > DSPS[j].DbPS())
+                    {
+                        PhanSo temp = DSPS[i];
+                        DSPS[i] = DSPS[j];
+                        DSPS[j] = temp;
+                    }
+                }
+            }
+        }
     } // End of class DSPhanSo
-    
-    
+
+
     // Main program 
     class Program
     {
         static void Main()
         {
             DSPhanSo ds = new DSPhanSo();
-            ds.NhapDS();
-            ds.XuatDS();
+            ds.Nhap();
+            ds.Xuat();
+            ds.Max();
+            ds.Adv();
+            ds.Xuat();
         }
     } // End of class Program
 }
---
+++ Point/Program.cs
@@ -0,0 +1,124 @@
+﻿using System;
+
+namespace Bai2_3
+{
+    //Khai báo lớp Point
+    public class Point
+    {
+        public double X{get;set;}
+        public double Y{get;set;}
+
+        public string? Color{get;set;}
+
+        public Point(double x = 0, double y = 0)
+        {
+            X = x;
+            Y = y;
+        }
+
+        public void Nhap()
+        {
+            Console.Write("Nhap hoanh do: ");
+            X = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Nhap tung do: ");
+            Y = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Nhap mau cho diem: ");
+            Color = Convert.ToString(Console.ReadLine());
+        }
+
+        public void Xuat()
+        {
+            Console.Write("{0},{1}",X,Y);
+        }
+
+        public void Move(double dx, double dy)
+        {
+            X += dx;
+            Y += dy;
+        }
+
+
+    } // End of Point
+    
+    public class DSPoint
+    {
+        public int SL;
+        Point[] DSP = null!;
+
+        public void Nhap()
+        {
+            Console.Write("Nhap so luong toa do: ");
+            SL = Convert.ToInt32(Console.ReadLine());
+            DSP = new Point[SL];
+            for(int i=0; i<SL; i++)
+            {   
+                Console.WriteLine("Nhap toa do thu {0}: ",i+1);
+                DSP[i] = new Point(0,0);
+                DSP[i].Nhap();
+            }
+        }
+        public void Xuat()
+        {
+            for(int i=0; i<SL; i++)
+            {
+                Console.Write("Toa do da nhap thu {0}: ",i+1);
+                DSP[i].Xuat();
+                Console.WriteLine("");
+            }
+        }
+
+        //Tim diem cach xa goc toa do nhat
+        private double KCPoint(Point p)
+        {
+            return Math.Sqrt(Math.Pow(p.X,2) + Math.Pow(p.Y,2));
+        }
+
+        public void MaxPoint()
+        {
+            double max = 0;
+            for(int i=1; i<SL; i++)
+            {
+                if(max<KCPoint(DSP[i]))
+                {
+                    max = i;
+                }
+            }
+            Console.Write("Toa do diem lon nhat la: ");
+            DSP[(int)max].Xuat();
+        }
+
+        public void MaxTwoP()
+        {
+            double max = Math.Abs(KCPoint(DSP[0]) - KCPoint(DSP[1]));
+            int temp1 = 0, temp2 = 0;
+            for(int i=0; i<SL-1; i++)
+            {
+                for(int j=i+1; j<SL; j++)
+                {
+                    if(max > Math.Abs(KCPoint(DSP[i]) - KCPoint(DSP[j])))
+                    {
+                        temp1 = i;
+                        temp2 = j;
+                    }
+                    Console.Write("\nToa do 2 diem gan nhat la: {0},{1}",i,j);
+                }
+            }
+            Console.Write("\nToa do 2 diem gan nhat la:");
+            DSP[(int)temp1].Xuat();
+            DSP[(int)temp2].Xuat();
+        }
+    }
+    
+    // Main program 
+    class Program
+    {
+        static void Main()
+        {
+            DSPoint ds = new DSPoint();
+            ds.Nhap();
+            ds.Xuat();
+            ds.MaxPoint();
+            ds.MaxTwoP();
+        }
+    } // End of class Program
+}
